49129189d68510ab25f3da77ccf060c74c04ca54
Pearce Moffat <pearce_moffat@hotmail.com>
2017-10-02 11:12:21 -0700

    message

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ var db = require("../models");
 
 module.exports = function(app) {
 
-    app.get("*", function(req, res){
+    app.get("/", function(req, res){
         
         res.render("index");
     });
